cad3708f01a6af158000ff34fac9aadbae172538
Daniel Canalda <danielcanalda@gmail.com>
2023-12-11 02:12:44 +0100

    cambios desde linia de comandos

--- helpers/generarJWT.js
+++ helpers/generarJWT.js
@@ -1,9 +1,11 @@
 import jwt from 'jsonwebtoken'
 
 const generarJWT = (id) => {
-    return jwt.sign( { id }, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: '30d'
     } )
 }
 
 export default generarJWT
+
+//Esto es solo un comentario
